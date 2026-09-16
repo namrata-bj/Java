743ce324615b23d23f93e527c76a3615ba56d1ce
Namrata Bharat Jadhav <namujadhav1717@gmail.com>
2026-09-16 20:08:28 +0530

    Add files via upload

---
+++ 01_java-basics practise codes/Division.java
@@ -0,0 +1,9 @@
+class Div{
+    public static void main(String[] args) {
+        int N1 = 100;
+        int N2 = 25;
+        int div ;
+        div = N1 / N2;
+        System.out.println("Division of this two no. "+div);
+    }
+}
---
+++ 01_java-basics practise codes/Substraction.java
@@ -0,0 +1,10 @@
+class Sub{
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 10;
+        int sub ;
+        sub = a - b;
+        System.out.println("Substraction of this two no. "+sub);
+        
+    }
+}

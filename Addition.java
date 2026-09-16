bafba173c519c96355160f93ba72745adcf1c36c
Namrata Bharat Jadhav <namujadhav1717@gmail.com>
2026-09-16 19:24:40 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+class Sum{
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 10;
+        int sum;
+        sum = a - b;
+        System.out.println("Substraction of this two no. "+sum); 
+    }
+}
